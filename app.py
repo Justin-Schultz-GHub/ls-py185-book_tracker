9740e98bf758845b6b657f88e8f7fe03d9767374
Justin-Schultz-GHub <SchultzJustin@ymail.com>
2026-03-14 00:55:20 +0000

    Begin creating sign up logic

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import secrets
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask import (
                     flash,
                     Flask,
@@ -23,10 +24,47 @@ app = Flask(__name__)
 app.config['DATABASE'] = 'flashcards'
 app.secret_key=secrets.token_hex(32)
 
+@app.before_request
+def initialize_persistence():
+    dbname = app.config.get('DATABASE', os.environ.get('DATABASE', 'book_tracker'))
+    g.storage = DatabasePersistence(dbname=dbname)
+
 # Route Hooks
 @app.route('/')
 def index():
+    if "user_id" in session:
+        return redirect(url_for('books'))
+
     return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if g.storage.username_exists(username):
+            flash('This username is already taken.', 'error')
+            return render_template('signup.html', username=username)
+
+        if not password:
+            flash('Please enter a password.', 'error')
+            return render_template('signup.html')
+
+        hashed_password = generate_password_hash(password)
+        g.storage.create_new_user(username, hashed_password)
+        user_id = g.storage.get_user_id(username)
+        session['user_id'] = user_id
+        flash('Account created successfully', 'success')
+
+        return redirect(url_for('books'))
+
+
+    return render_template('signup.html')
+
+@app.route('/books')
+def books():
+    return render_template('books.html')
 
 
 if __name__ == "__main__":
--- persistence/database_persistence.py
+++ persistence/database_persistence.py
@@ -9,4 +9,104 @@ logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 logger = logging.getLogger(__name__)
 
 class DatabasePersistence:
-    pass
+    def __init__(self, dbname=None):
+        self.dbname = dbname or os.environ.get('DATABASE') or 'book_tracker'
+        self._setup_schema()
+
+    def _setup_schema(self):
+        with self._database_connect() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('''
+                                CREATE TABLE IF NOT EXISTS books (
+                                id SERIAL PRIMARY KEY,
+                                title TEXT UNIQUE NOT NULL,
+                                author TEXT
+                                );
+                            ''')
+
+                cursor.execute('''
+                                CREATE TABLE IF NOT EXISTS genres (
+                                id SERIAL PRIMARY KEY,
+                                name text UNIQUE NOT NULL
+                                );
+                            ''')
+
+                cursor.execute('''
+                                CREATE TABLE IF NOT EXISTS books_genres
+                                book_id INTEGER NOT NULL REFERENCES books(id) ON DELETE CASCADE,
+                                genre_id INTEGER NOT NULL REFERENCES genres(id) ON DELETE CASCADE,
+                                PRIMARY KEY (book_id, genre_id)
+                                );
+                            ''')
+                cursor.execute('''
+                                CREATE INDEX IF NOT EXISTS idx_books_genres_genre_id
+                                ON books_genres (genre_id);
+                            ''')
+                cursor.execute('''
+                                CREATE TABLE IF NOT EXISTS users (
+                                id SERIAL PRIMARY KEY,
+                                username VARCHAR(20) UNIQUE,
+                                password_hash TEXT NOT NULL
+                                );
+                            ''')
+                cursor.execute('''
+                                CREATE TABLE IF NOT EXISTS users_books (
+                                id SERIAL PRIMARY KEY,
+                                user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+                                book_id INTEGER REFERENCES books(id) ON DELETE CASCADE,
+                                status TEXT NOT NULL CHECK (status in ('Completed', 'Reading', 'Plan to Read', 'Dropped')),
+                                score TEXT
+                                );
+                            ''')
+
+    @contextmanager
+    def _database_connect(self):
+        if os.environ.get('FLASK_ENV') == 'production':
+            connection = psycopg2.connect(os.environ['DATABASE_URL'])
+        else:
+            connection = psycopg2.connect(dbname=self.dbname)
+
+        try:
+            with connection:
+                yield connection
+        finally:
+            connection.close()
+
+    def username_exists(self, username):
+        query = '''
+                SELECT 1 FROM users
+                WHERE username = (%s);
+                '''
+        logger.info('Executing query: %s', query)
+
+        with self._database_connect() as connection:
+            with connection.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query, (username,))
+                result = cursor.fetchone()
+
+        return result is not None
+
+    def create_new_user(self, username, password):
+        query = '''
+                INSERT INTO users (username, password_hash)
+                values (%s, %s)
+                '''
+        logger.info('Executing query: %s', query)
+
+        with self._database_connect() as connection:
+            with connection.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query, (username, password,))
+
+    def get_user_id(self, username):
+        query = '''
+                SELECT id FROM users
+                WHERE username = (%s);
+                '''
+        logger.info('Executing query: %s', query)
+
+        with self._database_connect() as connection:
+            with connection.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query, (username,))
+                result = cursor.fetchone()
+
+        return result['id'] if result else None
